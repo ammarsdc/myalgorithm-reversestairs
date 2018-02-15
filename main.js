0450c6a53a6e4d8aaedb579ca9f981f1b2088ef5
ammarsdc <ammarsdc@IBNSANI-MBP.local>
2018-02-15 11:33:39 +0800

    The latest commit

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-var stage = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+var stairs = ["&nbsp;", "&nbsp;", "&nbsp;", "&nbsp;", "&nbsp;"];
 for(i = 4; i >= 0; i--) {
-    stage.charAt(i) = "#";
-    document.write(stage + "<br />");
+    stairs[i] = "#";
+    document.write(stairs.join("") + "<br />");
 }
